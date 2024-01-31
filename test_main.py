e29b69ef0d2c6d232a3addd0653f7df0491678bd
allgaiernoah <143101476+allgaiernoah@users.noreply.github.com>
2024-01-31 17:59:38 -0600

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -13,11 +13,8 @@ def test_binary_search():
 	assert binary_search([1,2,3,4,5], 5) == 4
 	assert binary_search([1,2,3,4,5], 1) == 0
 	assert binary_search([1,2,3,4,5], 6) == -1
-	### TODO: add two more tests here.
-	assert binary_search([1,2,3,4,5], 3) == 2
-	assert binary_search([1,2,3,4,5], 2) == 3
-
-	###
+	assert binary_search([1,2,3,4,5,6,7,8,9,10], 7) == 6
+	assert binary_search([1,2,3,4,5,6,7,8,9,10], 2) == 1
 
 
 def test_compare_search():
